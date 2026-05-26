58fc8f050f3a86600a4414242e1b25729e10363d
dieriba <dieriba.pro@gmail.com>
2026-05-26 14:10:27 +0200

    feat: setup clp parse to get user input command

--- src/main.c
+++ src/main.c
@@ -2,20 +2,20 @@
 
 typedef enum COMMAND
 {
-    CONTAINER,
+    JAILER,
+    CONTAINER_CMD,
     CONTAINER_CMD_RUN,
 } COMMAND;
 
 void container_cmd_run(Command *command)
 {
-    
 }
 
 void handle_user_command(Command *user_command)
 {
     switch (user_command->code)
     {
-    case CONTAINER:
+    case JAILER:
         break;
     case CONTAINER_CMD_RUN:
         container_cmd_run(user_command);
@@ -24,19 +24,37 @@ void handle_user_command(Command *user_command)
     }
 }
 
-int main(int argc, char **argv, char **envp)
+static Command init_container_command()
 {
-    Command *user_command;
-
-    Command root;
-    clp_init_command(&root, "container", "", CONTAINER);
+    Command container;
+    clp_init_command(&container, CONTAINER_CMD, "container", "Manage containers");
 
     Command run;
-    clp_init_command(&run, "run", "run a container", CONTAINER_CMD_RUN);
+    clp_init_command(&run, CONTAINER_CMD_RUN, "run", "Create and run a new container");
 
-    clp_add_command_sub_command(&root, &run);
+    Option run_cmd_opt_ports;
+    clp_init_option(&run_cmd_opt_ports, "port", "p", "Map a container's port to host ports", TYPE_STR, false, false);
+    Option run_cmd_opt_mount;
+    clp_init_option_kv(&run_cmd_opt_mount, "mount", "m", "Mount host specific path into container", false, false);
+    Option run_cmd_opt_network;
+    clp_init_option_kv(&run_cmd_opt_mount, "network", "n", "Manage container's network", false, false);
+    
+    clp_add_command_option(&run, &run_cmd_opt_ports);
+    clp_add_command_option(&run, &run_cmd_opt_mount);
+    clp_add_command_option(&run, &run_cmd_opt_network);
+
+    return container;
+}
+
+int main(int argc, char **argv, char **envp)
+{
+    Command root;
+    clp_init_command(&root, JAILER, "jailer", "");
+    Command container = init_container_command();
+
+    clp_add_command_sub_command(&root, &container);
+    Command *user_command = NULL;
     clp_parse_args(&root, argv, &user_command);
     handle_user_command(user_command);
-end:
     clp_cleanup(&root);
 }
