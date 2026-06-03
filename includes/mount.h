fa034e1a57c03ef820778d8f39c92d23a0c57dc6
dieriba <dieriba.pro@gmail.com>
2026-06-03 23:55:30 +0200

    refactor: use macro for option name instead of raw lit

--- includes/mount.h
+++ includes/mount.h
@@ -1,7 +1,8 @@
 #ifndef MOUNT_H
 #define MOUNT_H
 
-#define MOUNT_OPT "mount"
+#define MOUNT_LONG_NAME_OPT "mount"
+#define MOUNT_SHORT_NAME_OPT "m"
 #define MOUNT_OPT_KEY_SRC MOUNT_OPT "src"
 #define MOUNT_OPT_KEY_DST MOUNT_OPT "dst"
 
--- src/jailer/container.c
+++ src/jailer/container.c
@@ -4,9 +4,7 @@
 
 void container_run(Command *command)
 {
-    
 }
-
 
 Command init_container_command()
 {
@@ -19,10 +17,10 @@ Command init_container_command()
     Option run_cmd_opt_ports;
     clp_init_option(&run_cmd_opt_ports, "port", "p", "Map a container's port to host ports", TYPE_STR, false, false);
     Option run_cmd_opt_mount;
-    clp_init_option_kv(&run_cmd_opt_mount, "mount", "m", "Mount host specific path into container", false, false);
+    clp_init_option_kv(&run_cmd_opt_mount, MOUNT_LONG_NAME_OPT, MOUNT_SHORT_NAME_OPT, "Mount host specific path into container", false, false);
     Option run_cmd_opt_network;
     clp_init_option_kv(&run_cmd_opt_mount, "network", "n", "Manage container's network", false, false);
-    
+
     clp_add_command_option(&run, &run_cmd_opt_ports);
     clp_add_command_option(&run, &run_cmd_opt_mount);
     clp_add_command_option(&run, &run_cmd_opt_network);
