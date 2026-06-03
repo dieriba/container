a6467c0ca3a04637253cfe4d6baad8771a8d3db2
dieriba <dieriba.pro@gmail.com>
2026-06-04 00:11:23 +0200

    chore: remove unimplemented port and network options

--- src/jailer/container.c
+++ src/jailer/container.c
@@ -16,16 +16,11 @@ Command init_container_command()
     Command run;
     clp_init_command(&run, CONTAINER_CMD_RUN, "run", "Create and run a new container");
 
-    Option run_cmd_opt_ports;
-    clp_init_option(&run_cmd_opt_ports, "port", "p", "Map a container's port to host ports", TYPE_STR, false, false);
     Option run_cmd_opt_mount;
     clp_init_option_kv(&run_cmd_opt_mount, MOUNT_LONG_NAME_OPT, MOUNT_SHORT_NAME_OPT, "Mount host specific path into container", false, false);
-    Option run_cmd_opt_network;
-    clp_init_option_kv(&run_cmd_opt_mount, "network", "n", "Manage container's network", false, false);
 
-    clp_add_command_option(&run, &run_cmd_opt_ports);
+
     clp_add_command_option(&run, &run_cmd_opt_mount);
-    clp_add_command_option(&run, &run_cmd_opt_network);
 
     clp_add_command_sub_command(&container, &run);
 
